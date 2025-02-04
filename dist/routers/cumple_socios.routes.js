5592b2b259b65db974e9c8f4f3c89c05e32a11cd
joe molina <joedodaniljr123@gmail.com>
2025-02-04 09:35:11 -0400

    s

--- dist/routers/cumple_socios.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const cumple_socios_controller_1 = require("../controllers/cumple_socios.controller");
-const router = (0, express_1.Router)();
-router.get('/', cumple_socios_controller_1.PartnerBirthdays.getAll);
-exports.default = router;
