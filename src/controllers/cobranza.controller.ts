607eedbfdc3ecc67cfdf251598b9ceb2d533bd7d
joe molina <joedodaniljr123@gmail.com>
2025-02-21 14:22:53 -0400

    cambio build a dist package.json

--- src/controllers/cobranza.controller.ts
+++ src/controllers/cobranza.controller.ts
@@ -5,35 +5,60 @@ import nodemailer from 'nodemailer'
 
 export class Cobranza {
 
-  static async sentMail(req: any, res: any) {
-
-    const socios = req.body.data
+  static async sentMail(_req: any, res: any) {
 
     try {
+      const socios = await CobranzsaModel.getAllSocios()
+      const cargos = await CobranzsaModel.getCargosPendientes()
+      const mantenimiento = await CobranzsaModel.getAllMantenimientos()
+      const result = Object.groupBy(mantenimiento, ({ accion }) => accion)
+      const result2 = Object.groupBy(cargos, ({ accion }: { accion: any }) => accion)
+
+      const data = socios && socios.filter(socio => !socio.accion?.includes('D')).map((socio) => {
+
+        //cuota mantenimiento
+        const deudaSocio = deuda(result[socio.accion])
+        const total_divisa = deudaSocio.reduce((acc, cv) => acc + cv.divisa, 0)
+
+        //cargos
+        const cargos = result2[socio.accion]
+        const cargos_cantidad = cargos?.length
+        const cargos_divisa = cargos?.reduce((acc: any, cv: any) => acc + cv.divisa, 0)
+
+        return {
+          // info_personal: socio,
+          accion: socio.accion,
+          cedula: socio.cedula,
+          celular: socio.celular,
+          e_mail: socio.e_mail,
+          nombre: socio.nombre,
+          telefonos: socio.telefonos,
+          // cuotas_man: result[socio.accion],
+          ultimo_pago: fechaMasCercana(result[socio.accion]),
+          // deuda: deudaSocio,
+          cant_cuotas_vencidas: deudaSocio.length,
+          total_divisa,
+          cargos_divisa,
+          cargos_cantidad,
+          deuda_total: (cargos_divisa ? cargos_divisa : 0) + (total_divisa ? total_divisa : 0)
+        }
+      }).filter(socio => socio.cant_cuotas_vencidas > 1)
+
+      // const socios = req.body.data
+
       const enviarCorreo = async (data: any) => {
 
-        const text = `Estimado/a Socio/a 
-      ${data.nombre}:
-  
-  Reciba nuestros cordiales  saludos, en nombre del Depto. de Cobranzas de CASA DE ITALIA DE MARACAY. Le escribimos el dia de hoy para hacerle un amable recordatorio en relación a sus cuentas pendientes(vencidas) con nuestra Asociación, bajo el Nro. De Acción ${data.accion}
-  
-  El monto total pendiente,  asciende a $${data.deuda_total.toFixed(2)} USD, su pago equivalente en Bolívares (tasa BCV) al cambio del dia, correspondiente a ${data.cant_cuotas_vencidas} cuota(s) vencida(s).
-  
-  Es importante acotar, que debe asegurarse que todas las cuotas por diferencia(aumentos) estén canceladas, aunque es nuestro deber informarle.
-  
-  Es muy importante recibir oportunamente sus pagos, eso nos permite retribui en mantenimientos constantes y demás obligaciones de la asociación. 
-  
-  Cualquier información adicional puede solicitarla via WhatsApp al teléfono 0412-7863830, a la presente dirección de correo, en recepción hasta las 10pm o en nuestras oficinas administrativas hasta las 5pm.
-  
-  En caso de hacer pago móvil o transferencias debe enviarlas igualmente por esta vía para el debido registro y posterior emisión de su factura.
-  
-  Es para nosotros un placer poder recibirle. Confiamos en su absoluta disposición y contamos con su aporte a la brevedad. 
-  
-  Agradecemos además su atención.
-  
-  Maryelin Ruiz
-  Departamento de cobranzas
-      `
+        const text = `<p>
+    Estimado(a) Socio(a)<br/>
+    ${data.nombre}<br/>
+
+    Reciba nuestros cordiales  saludos, en nombre del Dpto. de Cobranzas de CASA DE ITALIA DE MARACAY. Le escribimos el dia de hoy para hacerle un amable recordatorio en relación a sus cuotas vencidas con nuestra Asociación, bajo el Nro. De Acción ${data.accion} <br/>
+
+    El monto total pendiente,  es de $${data.deuda_total.toFixed(2)}, su pago equivalente en Bolívares (tasa BCV) al cambio del día, correspondiente a ${data.cant_cuotas_vencidas} cuotas. <br/>
+
+    <img src="cid:cobranza" style="width: 200px; height: auto;">
+    Agradecemos además su atención.<br/>
+    atte. Dpto. de cobranzas<br/></p>`;
         // Configurar el transporte SMTP
         const transporter = nodemailer.createTransport({
           host: 'mail.casaitaliamaracay.com', // Cambia esto por el host SMTP de tu proveedor
@@ -47,33 +72,30 @@ export class Cobranza {
         // console.log(await transporter.verify())
         const info = await transporter.sendMail({
           from: `"Maryelin Ruiz" <cobranzas@casaitaliamaracay.com>`, // Remitente
-          to: `${data.e_mail}`, // Destinatario
+          // to: `${data.e_mail}`, // Destinatario
+          to: `joedodaniljr123@gmail.com`,
           cc: 'cobranzas@casaitaliamaracay.com',
           subject: 'Recordatorio de pagos de su accion en la CASA DE ITALIA DE MARACAY',
           text,
-          html: `
-            <p>Estimado/a Socio/a </p>
-            <p>${data.nombre}:</p>
-    
-    <p>Reciba nuestros cordiales  saludos, en nombre del Depto. de Cobranzas de CASA DE ITALIA DE MARACAY. Le escribimos el dia de hoy para hacerle un amable recordatorio en relación a sus cuentas pendientes(vencidas) con nuestra Asociación, bajo el Nro. De Acción ${data.accion}</p>
-    
-    <p>El monto total pendiente,  asciende a $${data.deuda_total.toFixed(2)} USD, su pago equivalente en Bolívares (tasa BCV) al cambio del dia, correspondiente a ${data.cant_cuotas_vencidas} cuota(s) vencida(s).</p>
-    
-    <p>Es importante acotar, que debe asegurarse que todas las cuotas por diferencia(aumentos) estén canceladas, aunque es nuestro deber informarle.</p>
-    
-    <p>Es muy importante recibir oportunamente sus pagos, eso nos permite retribuir en mantenimientos constantes y demás obligaciones de la asociación. </p>
-    
-    <p>Cualquier información adicional puede solicitarla via WhatsApp al teléfono 0412-7863830, a la presente dirección de correo, en recepción hasta las 10pm o en nuestras oficinas administrativas hasta las 5pm.</p>
-    
-    <p>En caso de hacer pago móvil o transferencias debe enviarlas igualmente por esta vía para el debido registro y posterior emisión de su factura.</p>
-    
-    <p>Es para nosotros un placer poder recibirle. Confiamos en su absoluta disposición y contamos con su aporte a la brevedad. </p>
-    
-    <p>Agradecemos además su atención.</p>
-    
-    <p>Maryelin Ruiz</p>
-    <p>Departamento de cobranzas</p>
-            `
+          html: `<p>
+    Estimado(a) Socio(a)<br/>
+    ${data.nombre}<br/><br/>
+
+    Reciba nuestros cordiales  saludos, en nombre del Dpto. de Cobranzas de CASA DE ITALIA DE MARACAY. Le escribimos el dia de hoy para hacerle un amable recordatorio en relación a sus cuotas vencidas con nuestra Asociación, bajo el Nro. De Acción ${data.accion} <br/><br/>
+
+    El monto total pendiente,  es de $${data.deuda_total.toFixed(2)}, su pago equivalente en Bolívares (tasa BCV) al cambio del día, correspondiente a ${data.cant_cuotas_vencidas} cuotas. <br/><br/>
+
+    <img src="cid:cobranza" style="width: 100%; height: auto; display: block;"><br/><br/>
+    Agradecemos además su atención.<br/><br/>
+    Este mensaje se genera de manera automatica</p>
+            `,
+          attachments: [
+            {
+              filename: 'cobranza.jpeg',
+              path: './assets/cobranza.jpeg', // Ruta local o URL
+              cid: 'cobranza' // mismo cid que usaste en el html
+            }
+          ]
         });
 
         return ((info.rejected.length == 0) ? true : false);
@@ -108,7 +130,7 @@ export class Cobranza {
         return resultados; // Devuelve el objeto con los resultados
       };
 
-      const resultados = await enviarCorreos(socios)
+      const resultados = await enviarCorreos(data || [])
 
       res.json(resultados)
     } catch (error) {
@@ -140,8 +162,6 @@ export class Cobranza {
         const cargos = result2[socio.accion]
         const cargos_cantidad = cargos?.length
         const cargos_divisa = cargos?.reduce((acc: any, cv: any) => acc + cv.divisa, 0)
-
-
 
         return {
           // info_personal: socio,
